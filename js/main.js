d5194b86adc108101b8cfea2cb06a2150b465a50
Xordas <120686481+Xordas@users.noreply.github.com>
2023-09-22 11:06:56 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -69,12 +69,5 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log('ugly');
   }
 
-  RewriteEngine on
 
-
-RewriteCond %{THE_REQUEST} /([^.]+)\.html [NC]
-RewriteRule ^ /%1 [NC,L,R]
-
-RewriteCond %{REQUEST_FILENAME}.html -f
-RewriteRule ^ %{REQUEST_URI}.html [NC,L]
 });
