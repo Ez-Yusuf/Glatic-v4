74f4823b1c54aa04a7c923af2eb354bae0a8f87e
mecharis-420 <121699952+mecharis-420@users.noreply.github.com>
2023-09-23 19:45:43 -0400

    edited prefix

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/~',
+    prefix: '/topic/lessons',
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
